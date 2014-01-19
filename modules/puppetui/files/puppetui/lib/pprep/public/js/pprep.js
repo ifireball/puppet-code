9d8772b2be236c5f1aa609cdb1a76615e442bc60
Barak Korren <barak.korren@gmail.com>
2014-01-20 00:21:14 +0200

    Added incremental search for computer in PPrep Also some navigation links

---
+++ modules/puppetui/files/puppetui/lib/pprep/public/js/pprep.js
@@ -0,0 +1,112 @@
+function filter_column(th, filter_func) {
+	var col_idx = th.closest('tr').children('th').index(th);
+	var hide_class = '_src_hidden' + col_idx;
+	var src_class = '_src_col' + col_idx;
+	// console.log("Found column at offset: " + col_idx);
+	if(!th.hasClass('_searchable')) {
+		th.addClass('_searchable');
+		th.closest('table')
+		.find('tbody > tr > :nth-child(' + col_idx + 1 + ')')
+		//.find('tbody tr td')
+		.addClass(src_class);
+		$('head').append('<style type="text/css">' + 
+			'tr.' + hide_class + ' { display: none; }' +
+			'</style>');
+	}
+	th.closest('table').find('.' + src_class)
+	.each(function(i, e_td) {
+		var td = $( e_td );
+		if(filter_func(td)) {
+			td.closest('tr').removeClass(hide_class);
+		} else {
+			td.closest('tr').addClass(hide_class);
+		}
+	});
+}
+
+function create_dd_menu() {
+	return $('<ul class="dropdown-menu pull-right" role="menu"></ul>');
+}
+
+function create_col_dd(menu) {
+	return $('<div class="btn-group btn-group-xs pull-right">' +
+		'<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">' +
+		'<span class="caret"></span>' +
+		'</button>' +
+		'</div>').append(menu).on('shown.bs.dropdown', function(e) {
+			$( this ).find('input').first().focus();
+		})
+}
+
+function add_dd_sort_items(menu) {
+	return menu.append('<li role="presentation" class="dropdown-header">Sort</li>').
+		append($('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ascending</a></li>')).
+		append($('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Descending</a></li>'));
+}
+
+function add_dd_search_item(menu) {
+	return menu.append('<li role="presentation" class="dropdown-header">Find</li>')
+		.append($('<li role="presentation">')
+			.append($('<form class="form-inline input-group input-group-sm" role="form" action="">')
+				.on('submit', function(e) {
+					e.preventDefault();
+					$( this ).closest('.btn-group')
+						.children('.dropdown-toggle').first()
+						.dropdown('toggle');
+				})
+				.append($('<input class="form-control" type="text" name="search" placeholder="Search..." value="">')
+					.click(function(e) {
+						e.stopPropagation();
+					})
+					.keyup(function(e) {
+						var $this = $( this );
+						var term = $this.val();
+						//console.log("serching: " + term)
+						$this.closest('.input-group').find('button')
+						.prop('disabled', term == '');
+						filter_column($this.closest('th'), function(td) {
+							return td.text().indexOf(term) >= 0;
+						});
+					})
+				)
+				.append($('<span class="input-group-btn">')
+					.append($('<button class="btn btn-default" type="button" disabled>' + 
+						'<span class="glyphicon glyphicon-remove"></span>' + 
+						'</button>')
+					).click(function(e) {
+						var $this = $( this );
+						$this.closest('.input-group')
+						.children(':text').val('')
+						.trigger('keyup');
+					})
+				)
+			)
+		);
+}
+
+function add_dd_calendar_item(menu) {
+	return menu;
+	// TODO
+}
+
+function educate_name_columns() {
+	$("th.tcol-name").append(create_col_dd(
+		add_dd_sort_items(add_dd_search_item(create_dd_menu()))
+	));
+}
+
+function educate_date_columns() {
+	$("th.tcol-date").append(create_col_dd(
+		add_dd_sort_items(add_dd_calendar_item(create_dd_menu()))
+	));
+}
+
+function educate_columns() {
+	educate_name_columns();
+	educate_date_columns();
+}
+
+$( document ).ready(function() {
+	educate_columns();
+});
+
