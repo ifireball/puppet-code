8a326b67c2a8ccbd3e576d40938a0ecb1af89d82
Barak Korren <barak.korren@gmail.com>
2014-03-07 17:49:17 +0200

    Adapted common code to run on Debian

--- modules/site/manifests/utils/vim.pp
+++ modules/site/manifests/utils/vim.pp
@@ -4,7 +4,10 @@
 #
 class site::utils::vim(
   $vimfiles = '/usr/share/vim/vimfiles',
-  $packages = [ 'vim-enhanced', 'vim-X11' ],
+  $packages = $::osfamily ? {
+    'RedHat' => [ 'vim-enhanced', 'vim-X11' ],
+    'Debian' => [ 'vim', 'vim-gnome' ], 
+  },
 ) {
   package { $packages:
     ensure => 'latest',
@@ -19,6 +22,8 @@ class site::utils::vim(
     mode => '644',
   }
   file {
+    [ $autoload, $plugin, ]:
+      ensure => 'directory';
     "$autoload/pathogen.vim":
       source => 'puppet:///modules/site/vim/pathogen/autoload/pathogen.vim';
     "$plugin/zzz-site-config.vim":
