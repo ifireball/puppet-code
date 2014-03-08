a847e86b2f964136d82c381839f95f11943b0bfc
Barak Korren <barak.korren@gmail.com>
2014-03-08 17:00:44 +0200

    Added support for setting up APT repositories Also included the 'stdlib' class to enable common stages

--- modules/site/manifests/common.pp
+++ modules/site/manifests/common.pp
@@ -3,6 +3,11 @@
 # Common configuration for all site servers
 #
 class site::common {
+  if $::osfamily == 'Debian' {
+    include apt
+  }
+  include stdlib 
+
   include site::puppet::agent
   include site::sudo
   include site::utils::shell
