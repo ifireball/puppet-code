b5826a188cda43b1d873d3e50bc7a43bddf629b8
Barak Korren <barak.korren@gmail.com>
2014-03-24 07:59:01 +0200

    Added search bar generation to pprep

--- modules/puppetui/files/puppetui/lib/pprep/datagrid.rb
+++ modules/puppetui/files/puppetui/lib/pprep/datagrid.rb
@@ -9,6 +9,7 @@ if RUBY_ENGINE == 'opal'
   require 'opal-haml'
   require 'template'
   require 'views/datagrid/column/header'
+  require 'views/datagrid/column/searchbar'
 end
 
 module UndefMethods
@@ -27,38 +28,78 @@ end
 class EnhancedElement
   include UndefMethods
 
-  def initialize(element = nil, &bl)
-    @element = element
+  def initialize(id = nil, &bl)
+    @id = id and @element = "##{id}"
     bl and SimpleDelegator.new(self).instance_eval(&bl)
   end
-  attr_accessor :element
-  def elm
-    @elm or raise "You should proably call 'attach' before trying GUI manipulations"
-  end
+  attr_accessor :id, :element
   def attach(element = nil)
-    @element = element || @element || ''
+    @element = element || @element || "##{id}"
     @element.is_a?(Element) || @element = Element[@element.to_s]
     raise RuntimeError, "Couldn't find element to attache to" if @element.empty?
     @elm = @element
-    undef_methods :attach, :element=
+    undef_methods :attach, :create, :element=
     enhance_element
+    self
   end
   def attached?
-    not @eln.nil?
+    not @elm.nil?
+  end
+  def create(id = nil)
+    @id = id || @id
+    attach(create_element)
+    elm
+  end
+  protected
+  def elm
+    @elm or raise "You should proably call 'attach' before trying GUI manipulations"
   end
   private
   def enhance_element
     puts "You should probably implement enhance_element for #{self.class}"
   end
+  def create_element
+    puts "You should probably implement create_element for #{self.class}"
+  end
 end
 
 class DataGrid < EnhancedElement
   class Column < EnhancedElement
-    def initialize(datagrid, title, element = nil, &bl)
+    class SearchBar < EnhancedElement
+      def initialize(column, &bl)
+        @column = column
+        super &bl
+      end
+      private
+      def enhance_element
+        @input = elm.find('input')
+        @btn = elm.find('button')
+        @input.on :keyup do
+          term = @input.value
+          @btn[:disabled] = term.empty?
+          @column.filter(term)
+        end
+        @btn.on :click do
+          @input.value = ''
+          @input.trigger :keyup
+        end
+      end
+      def create_element
+        Template['views/datagrid/column/searchbar'].render(self)
+      end
+    end
+
+    def initialize(datagrid, title, id = nil, &bl)
       @datagrid, @title = datagrid, title
-      super element, &bl
+      super id, &bl
     end
     attr_reader :title, :sorting
+    def search_bar
+      @search_bar ||= DataGrid::Column::SearchBar.new(self)
+    end
+    def filter(filter)
+      puts "You should probably implement filter for #{self.class}"
+    end
     private
     def enhance_element
       elm.html = Template['views/datagrid/column/header'].render(self)
@@ -85,11 +126,16 @@ class DataGrid < EnhancedElement
     end
   end
   def create_search_bar
+    @search_bar = Element['<tr class="search-bar">']
+    elm.find('thead') << @search_bar
+    columns.each do |col|
+      @search_bar << col.search_bar.create
+    end
   end
 end
 
 if RUBY_ENGINE == 'opal'
-  dg = DataGrid.new '#reports' do
+  dg = DataGrid.new 'reports' do
     column 'Report Time'
     column 'Status'
   end
