fc806f46696eb222bc3a1c9da681a8e00be1ab5f
Barak Korren <barak.korren@gmail.com>
2014-04-25 11:58:00 +0300

    Refactored QHaml into multiple resource files

--- modules/utils/files/qhaml/qhaml.rb
+++ modules/utils/files/qhaml/qhaml.rb
@@ -54,10 +54,6 @@ post '/viewer' do
   page_viewer
 end
 
-get '/style' do
-  scss :style
-end
-
 get %r{^/__opal_source_maps__/(.*)} do 
   path_info = params[:captures].first
   logger.info "Source map requested: #{path_info}"
@@ -83,6 +79,10 @@ module Sinatra
   register Sinatra::AssetPipeline
 end
 
+configure :development do
+  Sprockets::Helpers.expand = true
+end
+
 helpers do
   def page_viewer
     haml @project[:src], :layout => !@project[:src].match(/\A\s*!!!/)
@@ -91,121 +91,12 @@ end
 
 __END__
 
-@@frameset
-!!! Frameset
-%html
-  %head
-    %title QHaml
-  %frameset{:rows => "60%,*"}
-    %frame{:src => "/viewer", :name => "viewer"}
-    %frame{:src => "/editor", :name => "editor"}
-
-@@layout
-!!!
-%html
-  %head
-    %title QHtml Editor
-    %meta{:content => "width=device-width, initial-scale=1.0", :name => "viewport"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1-beta1/jquery.min.js"}
-    %link{:rel => "stylesheet", :href => "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}
-    %script{:src => "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"}
-    %link{:rel => "stylesheet", :href => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.css"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/codemirror.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/keymap/vim.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/xml/xml.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/css/css.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/htmlmixed/htmlmixed.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/javascript/javascript.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/ruby/ruby.min.js"}
-    %script{:src => "//cdnjs.cloudflare.com/ajax/libs/codemirror/4.0.3/mode/haml/haml.min.js"}
-    %link{:rel => "stylesheet", :href => "/style"}
-  %body
-    = yield
-
-@@editor
-%form.app-layout{:method => "post", :action => "/viewer", :target => "viewer"}
-  .navbar.navbar-default.app-layout-navbar
-    .container-fluid
-      .navbar-header
-        %button.navbar-toggle{:type => 'button', :data => {:toggle => 'collapse', :target => '#navbar-collapse'}}
-          %span.sr-only Toggle Navigation
-          %span.icon-bar
-          %span.icon-bar
-          %span.icon-bar
-        %span.navbar-brand QHaml
-      .collapse.navbar-collapse#navbar-collapse
-        .navbar-form.navbar-left
-          .form-group
-            %input.form-control.btn.btn-default{:type => 'reset', :value => 'Clear'}
-          .form-group
-            %label.control-label.sr-only{:for => 'language'}
-              Language:
-            %select.form-control#language{:name => 'language'}
-              %option{:selected => true} HAML
-              %option OPAL
-        .navbar-form.navbar-right
-          .form-group
-            %input.form-control.btn.btn-default{:type => 'submit', :value => 'Evaluate'}
-  .app-layout-content
-    %textarea.main-editor{:name => :src, :rows => 10}= @project[:src]
-:javascript
-  $(document).ready(function() {
-    CodeMirror.fromTextArea($('textarea.main-editor').get(0), {
-      mode: "text/x-haml",
-      vimMode: true,
-      lineNumbers: true,
-    });
-  });
- 
 @@default_src
 .container 
   .row
     .jumbotron
       %h1 QHaml
       %p Just type HAML into the form below and click submit
-
-@@style
-.app-layout {
-  position: fixed;
-  left: 0px;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  width: auto;
-  height: auto;
-  overflow: hidden;
-}
-.app-layout-navbar {
-  position: absolute;
-  left: 0px;
-  top: 0px;
-  right: 0px;
-  width: auto;
-  height: 50px;
-  overflow: hidden;
-}
-.app-layout-content {
-  position: absolute;
-  left: 0px;
-  top: 50px;
-  right: 0px;
-  bottom: 0px;
-  width: auto;
-  height: auto;
-  overflow: hidden;
-}
-.CodeMirror {
-  height: 100%;
-  width:100%;
-}
-.main-editor {
-  font-family: monospace;
-  height: 100%;
-  width:100%;
-  resize: none;
-  border: none;
-  overflow: auto;
-}
 
 @@opal_wrapper
 require 'opal'
